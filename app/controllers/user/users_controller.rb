b2b7da6545e77d01994fce2b633a65364197e20c
yurika0501 <titizuzu0501@gmail.com>
2023-03-05 11:50:50 +0000

    [Add]ゲストログイン機能

---
+++ app/controllers/user/users_controller.rb
@@ -0,0 +1,14 @@
+class User::UsersController < ApplicationController
+    before_action :ensure_guest_user, only: [:edit]
+    
+    
+    private
+    
+    # ゲストユーザーならプロフィールの編集を出来ないようにする
+    def ensure_guest_user
+        @user = User.find(params[:id])
+        if @user.name == "guestuser"
+            redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,15 @@ Rails.application.routes.draw do
     registrations: "user/registrations",
     sessions: 'user/sessions'
   }
-
+  scope module: :user do
+    root to: 'homes#top'
+    resources :posts
+    resource :users
+  end
+  
+  devise_scope :user do
+    post 'user/guest_sign_in', to: 'user/sessions#guest_sign_in'
+  end
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
