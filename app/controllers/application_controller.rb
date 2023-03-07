e1e5ea8eaf8a54a6a7b4b19fbdeb9ec14f206876
yurika0501 <titizuzu0501@gmail.com>
2023-03-07 02:32:22 +0000

    [Add]topページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    
+    private
+    def after_sign_out_path_for(resource_or_scope)
+        if resource_or_scope == :user
+            root_path
+        elsif resource_or_scope == :admin
+            new_admin_session_path
+        else
+            new_user_registration_path
+        end
+    end
 end
